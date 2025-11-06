8dacb4abd2cde13ef31547b262fce31142185ec2
Kevin G. Ferreira <kevingarciaf.kf@outlook.com>
2025-11-06 14:13:07 -0300

    adjusting to works at Render

--- W1-Project/swagger.js
+++ W1-Project/swagger.js
@@ -1,12 +1,15 @@
 const swaggerAutogen = require("swagger-autogen")();
+const env = process.env.NODE_ENV || "development";
 
 const doc = {
     info: {
         title: "Contacts Api",
         description: "Contacts Api"
     },
-    host: "localhost:4000",
-    schemes: ["http", "https"],
+    //host: "localhost:4000",
+    host: env === "production" ? "https://cse341-project01-t6b1.onrender.com" : "localhost:4000",
+    //schemes: ["http", "https"],
+    schemes: env === "production" ? ["https"] : ["http"],
     basePath: "/",
 };
 
